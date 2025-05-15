9819fa3028c4a0ed1737961ce6f4ebe57ba6f9d7
Rufus Victor <arulrufus@gmail.com>
2025-05-15 17:56:17 +0530

    Create FruitList.java

---
+++ FruitList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class FruitList {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Mango");
+        fruits.add("Orange");
+
+        System.out.println("Third fruit: " + fruits.get(2));
+    }
+}
